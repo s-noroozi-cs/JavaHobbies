c24f4db094a301ea55edc41f1000a78d3d02c4c7
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-04-15 11:00:16 +0430

    add new project

---
+++ spring-boot-proxy-aspect/src/main/java/com/hobi/proxyaspect/ProxyAspectApplication.java
@@ -0,0 +1,13 @@
+package com.hobi.proxyaspect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyAspectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyAspectApplication.class, args);
+	}
+
+}
---
+++ spring-boot-proxy-aspect/src/test/java/com/hobi/proxyaspect/ProxyAspectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobi.proxyaspect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyAspectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
