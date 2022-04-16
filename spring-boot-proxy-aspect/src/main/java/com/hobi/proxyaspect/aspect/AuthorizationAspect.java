061faff2ee880c9cda7832308ac4681f39c3ee6f
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-04-16 20:45:17 +0430

    complete aspect

--- spring-boot-proxy-aspect/src/main/java/com/hobi/proxyaspect/aspect/AuthorizationAspect.java
+++ spring-boot-proxy-aspect/src/main/java/com/hobi/proxyaspect/aspect/AuthorizationAspect.java
@@ -4,8 +4,10 @@ import com.hobi.proxyaspect.annotation.Authorization;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class AuthorizationAspect {
 
     @Before("@annotation(authorization)")
