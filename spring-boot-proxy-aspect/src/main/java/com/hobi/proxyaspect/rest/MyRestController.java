d9296af4de92dc90fce3b7845a70094f6d783f12
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-04-16 19:44:02 +0430

    Update MyRestController.java

--- spring-boot-proxy-aspect/src/main/java/com/hobi/proxyaspect/rest/MyRestController.java
+++ spring-boot-proxy-aspect/src/main/java/com/hobi/proxyaspect/rest/MyRestController.java
@@ -1,5 +1,16 @@
 package com.hobi.proxyaspect.rest;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
 @RestController
 public class MyRestController {
+
+    @GetMapping
+    public ResponseEntity<String> generateUUID(){
+        return ResponseEntity.ok(UUID.randomUUID().toString());
+    }
 }
