091054ef86c5fd5903265e957992657052f7c8d6
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-04-17 22:34:31 +0430

    Update AuthorizationInterceptor.java

--- spring-boot-proxy-aspect/src/main/java/com/hobi/proxyaspect/interceptor/AuthorizationInterceptor.java
+++ spring-boot-proxy-aspect/src/main/java/com/hobi/proxyaspect/interceptor/AuthorizationInterceptor.java
@@ -1,6 +1,19 @@
 package com.hobi.proxyaspect.interceptor;
 
-import org.springframework.web.servlet.handler.WebRequestHandlerInterceptorAdapter;
+import com.hobi.proxyaspect.annotation.Authorization;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
 
-public class AuthorizationInterceptor implements WebRequestHandlerInterceptorAdapter {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AuthorizationInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HandlerMethod methodHandle = (HandlerMethod)handler;
+        if (methodHandle.getMethod().isAnnotationPresent(Authorization.class)){
+
+        }
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
 }
