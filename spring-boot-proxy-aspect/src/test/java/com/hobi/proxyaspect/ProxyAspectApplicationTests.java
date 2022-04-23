25799dc2ee5881b81f656b8d8a6206f20645922e
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-04-23 18:35:32 +0430

    update logging configuration

--- spring-boot-proxy-aspect/src/test/java/com/hobi/proxyaspect/ProxyAspectApplicationTests.java
+++ spring-boot-proxy-aspect/src/test/java/com/hobi/proxyaspect/ProxyAspectApplicationTests.java
@@ -1,13 +1,16 @@
 package com.hobi.proxyaspect;
 
+import com.hobi.proxyaspect.logging.MemoryAppenderInstance;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProxyAspectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(MemoryAppenderInstance.getInstance().getEvents().size() > 0);
+    }
 
 }
