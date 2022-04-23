fcce62887c6620834b2460fd55de899d8d4f3453
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-04-23 18:26:17 +0430

    Create MemoryAppender.java

---
+++ spring-boot-proxy-aspect/src/main/java/com/hobi/proxyaspect/logging/MemoryAppender.java
@@ -0,0 +1,31 @@
+package com.hobi.proxyaspect.logging;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemoryAppender extends AppenderBase<ILoggingEvent>{
+    private final List<ILoggingEvent> events = new ArrayList<>();
+
+    @Override
+    protected void append(ILoggingEvent event) {
+        synchronized (events) {
+            events.add(event);
+        }
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        //MemoryAppenderInstance.setInstance(this);
+    }
+
+    @Override
+    public void stop() {
+        //MemoryAppenderInstance.setInstance(null);
+        super.stop();
+        events.clear();
+    }
+}
