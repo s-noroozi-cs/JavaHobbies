7e58da8e40605a617f945b1e7949307a05c5bb35
Saeid Noroozi <s-noroozi.cs@gmail.com>
2022-04-27 19:50:03 +0430

    create project for test container

---
+++ test-container/src/test/java/OracleDBTest.java
@@ -0,0 +1,26 @@
+import org.junit.ClassRule;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.OracleContainer;
+
+public class OracleDBTest {
+    private static final String oracleDockerImage = "gvenzl/oracle-xe:21.3.0-slim";
+    private static final String oracleSysPass = "sys";
+
+    @ClassRule
+    private static final OracleContainer oracleContainer = new OracleContainer(oracleDockerImage)
+            .withEnv("ORACLE_PASSWORD",oracleSysPass);
+
+    @BeforeAll
+    static void startUp(){
+        oracleContainer.start();
+    }
+
+    @Test
+    void checkOracleContainer() {
+        Assertions.assertNotNull(oracleContainer);
+        LoggerFactory.getLogger(this.getClass()).info(oracleContainer.getJdbcUrl());
+    }
+}
