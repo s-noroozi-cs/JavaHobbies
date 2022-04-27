56e6a15c9f3a9fb9283bc1aa3fcc0013621c225b
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-04-27 23:55:16 +0430

    redis container

--- test-container/src/test/java/RedisContainerTest.java
+++ test-container/src/test/java/RedisContainerTest.java
@@ -1,9 +1,11 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+import redis.clients.jedis.Jedis;
 
 @Testcontainers
 public class RedisContainerTest {
@@ -13,13 +15,34 @@ public class RedisContainerTest {
     private static GenericContainer redisContainer =
             new GenericContainer(DockerImageName.parse(redisDockerImage))
                     .withExposedPorts(6379);
+    private static Jedis jedis;
+    private static final String redisKey= "myKey";
+    private static final String redisValue = "myValue";
 
+    @BeforeAll
+    static void initRedisClient(){
+        jedis = new Jedis(redisContainer.getHost(),redisContainer.getFirstMappedPort());
+    }
 
     @Test
     void checkRedisContainer()throws Exception{
         Assertions.assertNotNull(redisContainer);
         Assertions.assertNotNull(redisContainer.getContainerId());
+        Assertions.assertNotNull(jedis);
+        Assertions.assertNotNull(jedis.ping());
     }
+
+    @Test
+    void writeMessage(){
+        Assertions.assertEquals(jedis.set(redisKey,redisValue),"OK");
+    }
+
+    @Test
+    void readMessage(){
+        Assertions.assertEquals(jedis.get(redisKey),redisValue);
+    }
+
+
 
 
 
