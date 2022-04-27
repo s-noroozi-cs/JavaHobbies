9c6a7c5c4753b38fcf623344750690873d760046
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-04-27 23:28:26 +0430

    Update RedisContainerTest.java

--- test-container/src/test/java/RedisContainerTest.java
+++ test-container/src/test/java/RedisContainerTest.java
@@ -1,2 +1,27 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
 public class RedisContainerTest {
+    private static final String redisDockerImage = "redis:6.2.6";
+
+    @Container
+    private static GenericContainer redisContainer =
+            new GenericContainer(DockerImageName.parse(redisDockerImage))
+                    .withExposedPorts(6379);
+
+
+    @Test
+    void checkRedisContainer()throws Exception{
+        Assertions.assertNotNull(redisContainer);
+        Assertions.assertNotNull(redisContainer.getContainerId());
+    }
+
+
+
+
 }
