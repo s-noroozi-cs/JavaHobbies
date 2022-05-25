3ec9295c9629025f706567ef363d17a2b2ce35cf
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-05-25 23:24:43 +0430

    Update KafkaContainerTest.java

--- test-container/src/test/java/KafkaContainerTest.java
+++ test-container/src/test/java/KafkaContainerTest.java
@@ -8,27 +8,49 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class KafkaContainerTest {
     private static final String zookeeperDockerImage = "zookeeper:3.7.0";
+    private static final String kafkaDockerImage = "wurstmeister/kafka:2.13-2.8.1";
 
+    private static final int kafkaPort = getLocalFreeRandomPort();
     private static GenericContainer zookeeperContainer;
 
     private static GenericContainer kafkaContainer;
 
+
+
     @BeforeAll
-    static void init_container(){
+    static void init_container() {
 
         zookeeperContainer = new GenericContainer(DockerImageName.parse(zookeeperDockerImage))
                 .withExposedPorts(2181);
         zookeeperContainer.start();
 
+
+        int kaf
+        kafkaContainer = new GenericContainer(DockerImageName.parse(kafkaDockerImage))
+                .withExposedPorts()
+        ;
+
+    }
+
+    private static int getLocalFreeRandomPort() {
+        try {
+            ServerSocket server = new ServerSocket(0);
+            int port = server.getLocalPort();
+            server.close();
+            return port;
+        } catch (Throwable e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
     }
 
     @Test
-    void test_zookeeper_container_init(){
+    void test_zookeeper_container_init() {
         Assertions.assertNotNull(zookeeperContainer);
         Assertions.assertNotNull(zookeeperContainer.getContainerId());
         Assertions.assertNotNull(zookeeperContainer.getHost());
@@ -36,30 +58,30 @@ public class KafkaContainerTest {
     }
 
     @Test
-    void test_zookeeper_container_command(){
+    void test_zookeeper_container_command() {
         try {
             Socket socket = new Socket(zookeeperContainer.getHost(), zookeeperContainer.getFirstMappedPort());
             OutputStream os = socket.getOutputStream();
             InputStream is = socket.getInputStream();
 
 
-            PrintWriter printWriter = new PrintWriter( new OutputStreamWriter(socket.getOutputStream()));
+            PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
             printWriter.print("srvr");
             printWriter.flush();
 
-            while (is.available() == 0){
+            while (is.available() == 0) {
                 Thread.sleep(100);
             }
 
-            String out = new String(is.readAllBytes(),StandardCharsets.US_ASCII);
+            String out = new String(is.readAllBytes(), StandardCharsets.US_ASCII);
 
             Assertions.assertTrue(out.contains("Zookeeper version: "));
             Assertions.assertTrue(out.contains("Latency min/avg/max: "));
             Assertions.assertTrue(out.contains("Received: "));
             Assertions.assertTrue(out.contains("Sent: "));
             Assertions.assertTrue(out.contains("Connections: "));
-        }catch (Throwable ex){
-            Assertions.fail(ex.getMessage(),ex);
+        } catch (Throwable ex) {
+            Assertions.fail(ex.getMessage(), ex);
         }
     }
 
