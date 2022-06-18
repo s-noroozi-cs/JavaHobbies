31f177cd633d0dfbe24b4959a80c22d03f06f767
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-06-18 23:21:40 +0430

    add test to create student

--- spring-boot-db-test-contaoner/src/main/java/com/hobi/dbtest/controller/StudentController.java
+++ spring-boot-db-test-contaoner/src/main/java/com/hobi/dbtest/controller/StudentController.java
@@ -3,12 +3,15 @@ package com.hobi.dbtest.controller;
 import com.hobi.dbtest.entity.Student;
 import com.hobi.dbtest.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/students")
+@RestController
+@RequestMapping("/api/v1/students")
 public class StudentController {
     private StudentService service;
 
@@ -18,7 +21,7 @@ public class StudentController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewStudent(@RequestBody Student student) {
-        return ResponseEntity.ok(service.create(student));
+    public ResponseEntity<Student> saveNewStudent(@RequestBody Student student) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(student));
     }
 }
--- spring-boot-db-test-contaoner/src/test/java/com/hobi/dbtest/DbTestApplicationTests.java
+++ spring-boot-db-test-contaoner/src/test/java/com/hobi/dbtest/DbTestApplicationTests.java
@@ -1,13 +1,37 @@
 package com.hobi.dbtest;
 
+import com.hobi.dbtest.entity.Student;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DbTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+    private RestTemplate restTemplate = new RestTemplate();
+
+    private String getStudentsUrl() {
+        return "http://localhost:" + port + "/api/v1/students";
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void test_rest_api_call() {
+        Student student = new Student();
+        student.setName("Ali");
+        student.setAge(25);
+        ResponseEntity<Student> responseEntity =
+                restTemplate.postForEntity(getStudentsUrl(), student, Student.class);
+        Assertions.assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+    }
 
 }
