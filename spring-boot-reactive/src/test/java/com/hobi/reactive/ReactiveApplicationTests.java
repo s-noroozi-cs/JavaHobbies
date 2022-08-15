851ef0d3f48082f29ebe32030cf9784d40b42758
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-08-15 22:05:32 +0430

    Update ReactiveApplicationTests.java

--- spring-boot-reactive/src/test/java/com/hobi/reactive/ReactiveApplicationTests.java
+++ spring-boot-reactive/src/test/java/com/hobi/reactive/ReactiveApplicationTests.java
@@ -1,13 +1,23 @@
 package com.hobi.reactive;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ReactiveApplicationTests {
+
+	@LocalServerPort
+	private int port;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void check_server_port(){
+		Assertions.assertTrue(port > 0);
 	}
 
 }
