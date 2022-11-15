2f37075a3f9022c6a34e096a4a82c5fd99120ae4
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-11-15 22:52:28 +0330

    find conflict :-)

--- spring-boot-consul-registry-discovery/src/main/java/com/example/demo/DemoApplication.java
+++ spring-boot-consul-registry-discovery/src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,32 @@
 package com.example.demo;
 
+import com.ecwid.consul.v1.ConsulRawClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.consul.ConsulAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Supplier;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public Supplier<Long> numberProducer() {
+        Class c = ConsulAutoConfiguration.class;
+        return () -> System.currentTimeMillis();
+    }
+
+    @Bean
+    public Supplier<ConsulRawClient.Builder> consulRawClientBuilderSupplier() {
+        return ConsulAutoConfiguration.createConsulRawClientBuilder();
+    }
 
 }
--- spring-boot-consul-registry-discovery/src/test/java/com/example/demo/DemoApplicationTests.java
+++ spring-boot-consul-registry-discovery/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception{
+		System.out.println("waiting to registration ...");
+		Thread.sleep(10_000L);
 	}
 
 }
