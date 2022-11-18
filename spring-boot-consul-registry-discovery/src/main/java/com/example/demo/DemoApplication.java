1694df2eef4c1430f4de5726a8764ca879c0ca6e
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-11-18 11:32:27 +0330

    Update DemoApplication.java

--- spring-boot-consul-registry-discovery/src/main/java/com/example/demo/DemoApplication.java
+++ spring-boot-consul-registry-discovery/src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import com.ecwid.consul.v1.ConsulRawClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.consul.ConsulAutoConfiguration;
 import org.springframework.context.annotation.Bean;
