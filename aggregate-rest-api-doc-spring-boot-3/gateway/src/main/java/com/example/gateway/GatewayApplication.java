50d9c317d1dedb951d2ee42045b3d88dcdd1a55c
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-11-25 16:45:05 +0330

    commit

--- aggregate-rest-api-doc-spring-boot-3/gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ aggregate-rest-api-doc-spring-boot-3/gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -2,9 +2,21 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+@RequestMapping("/api/v1")
 public class GatewayApplication {
+
+	@GetMapping("/test")
+	public String test(){
+		return "ok";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
--- aggregate-rest-api-doc-spring-boot-3/gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
+++ aggregate-rest-api-doc-spring-boot-3/gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GatewayApplicationTests {
 
 	@Test
