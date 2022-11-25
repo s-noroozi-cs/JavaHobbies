df4467543d30c3a33e877ad16669d8d27586b8ed
Saeid Noroozi <s.noroozi.cs@gmail.com>
2022-11-25 20:04:04 +0330

    commit

--- aggregate-rest-api-doc-spring-boot-3/gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ aggregate-rest-api-doc-spring-boot-3/gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -1,42 +1,50 @@
 package com.example.gateway;
 
-import jakarta.annotation.PostConstruct;
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.beans.MutablePropertyValues;
-import org.springframework.beans.factory.BeanFactory;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class GatewayApplication {
 
     @Autowired
-    private ConfigurableBeanFactory context;
-    @Autowired
     private DiscoveryClient discoveryClient;
 
-    @Bean
-    public GroupedOpenApi gatewayGroup() {
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @GetMapping(value = "/v3/api-docs/swagger-config", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> swaggerConfig() {
+        JsonObject js = new JsonObject();
+        js.addProperty("configUrl", "/v3/api-docs/swagger-config");
+
+        JsonArray jsUrls = new JsonArray();
         for (String name : discoveryClient.getServices()) {
-            GroupedOpenApi bean = GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
-            context.registerSingleton("groupedOpenApi_" + name, bean);
+            if (name.equals(applicationName) || name.equals("consul")) {
+                //ignore
+            } else {
+                JsonObject url = new JsonObject();
+                url.addProperty("url", "/%s/v3/api-docs".formatted(name));
+                url.addProperty("name", name);
+
+                jsUrls.add(url);
+            }
         }
 
-        return GroupedOpenApi.builder().pathsToMatch("/**").group("Gateway").build();
+        js.add("urls", jsUrls);
+        return ResponseEntity.ok(js.toString());
     }
-
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
--- aggregate-rest-api-doc-spring-boot-3/gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
+++ aggregate-rest-api-doc-spring-boot-3/gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
@@ -35,15 +35,15 @@ class GatewayApplicationTests {
 	void contextLoads() {
 	}
 
-	@Test
-	@SneakyThrows
-	void check_open_api_json() {
-		HttpResponse<String> response = sendGetRequest("/v3/api-docs");
-		Assertions.assertEquals(HttpStatus.OK.value(), response.statusCode());
-
-		JSONObject js = new JSONObject(response.body());
-		JSONObject paths = js.getJSONObject("paths");
-	}
+//	@Test
+//	@SneakyThrows
+//	void check_open_api_json() {
+//		HttpResponse<String> response = sendGetRequest("/v3/api-docs");
+//		Assertions.assertEquals(HttpStatus.OK.value(), response.statusCode());
+//
+//		JSONObject js = new JSONObject(response.body());
+//		JSONObject paths = js.getJSONObject("paths");
+//	}
 
 	@Test
 	void check_swagger_ui_redirect(){
