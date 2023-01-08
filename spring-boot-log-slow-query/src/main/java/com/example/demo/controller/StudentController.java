6c26d43b66747f2e9179df8b65f4224181c2358b
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-01-08 21:46:57 +0330

    remove un-used code

--- spring-boot-log-slow-query/src/main/java/com/example/demo/controller/StudentController.java
+++ spring-boot-log-slow-query/src/main/java/com/example/demo/controller/StudentController.java
@@ -9,8 +9,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//import javax.servlet.http.HttpServletRequest;
-
 import java.net.URI;
 import java.util.Optional;
 
--- spring-boot-log-slow-query/src/main/java/com/example/demo/entity/Student.java
+++ spring-boot-log-slow-query/src/main/java/com/example/demo/entity/Student.java
@@ -5,9 +5,6 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
-
 @Getter
 @Setter
 @Entity
