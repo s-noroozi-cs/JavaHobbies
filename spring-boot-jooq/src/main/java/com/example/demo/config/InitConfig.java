05211b76d0c8f3409ce9081eef8c6f67ebd6a5f1
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-01-13 15:08:17 +0330

    create project structure

---
+++ spring-boot-jooq/src/main/java/com/example/demo/config/InitConfig.java
@@ -0,0 +1,47 @@
+package com.example.demo.config;
+
+import org.jooq.impl.DataSourceConnectionProvider;
+import org.jooq.impl.DefaultConfiguration;
+import org.jooq.impl.DefaultDSLContext;
+import org.jooq.impl.DefaultExecuteListenerProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class InitConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public DataSourceConnectionProvider connectionProvider() {
+        return new DataSourceConnectionProvider
+                (new TransactionAwareDataSourceProxy(dataSource));
+    }
+
+    @Bean
+    public ExceptionTranslator exceptionTransformer() {
+        return new ExceptionTranslator();
+    }
+
+    public DefaultConfiguration configuration() {
+        DefaultConfiguration jooqConfiguration = new DefaultConfiguration();
+        jooqConfiguration.set(connectionProvider());
+        jooqConfiguration
+                .set(new DefaultExecuteListenerProvider(exceptionTransformer()));
+
+        return jooqConfiguration;
+    }
+
+    @Bean
+    public DefaultDSLContext dsl() {
+        return new DefaultDSLContext(configuration());
+    }
+
+}
