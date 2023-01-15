bcd8e3e5b59e94f1c4919d37a59312ffb18a982d
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-01-15 21:43:35 +0330

    Update DemoApplicationTests.java

--- spring-boot-query-dsl/src/test/java/com/example/demo/DemoApplicationTests.java
+++ spring-boot-query-dsl/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,6 +4,9 @@ import com.example.demo.model.QCity;
 import com.example.demo.repository.CityRepository;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,14 +15,24 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 @Slf4j
 class DemoApplicationTests {
-//	@PersistenceContext
-//	private EntityManager entityManager;
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@Autowired
 	private CityRepository cityRepository;
 
 	@Test
 	void contextLoads() {
+
+	}
+
+	@Test
+	void exec_query_using_query_factory(){
+		JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+		var city = QCity.city;
+		queryFactory.selectFrom(city)
+				.where(city.name.like("test"))
+				.fetchAll();
 	}
 
 	@Test
