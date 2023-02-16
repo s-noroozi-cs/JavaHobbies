80a6fb75fbc0ecf4f4c0d97336fa96056c4e994b
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-02-16 12:27:31 +0330

    add config util test

---
+++ keycloak-client-rest-api/keycloak-client-rest-api/src/test/java/com/keycloak/client/reasapi/ConfigUtilTest.java
@@ -0,0 +1,62 @@
+package com.keycloak.client.reasapi;
+
+import com.keycloak.client.restapi.ConfigUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
+import uk.org.webcompere.systemstubs.jupiter.SystemStub;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
+
+import java.util.UUID;
+
+@ExtendWith(SystemStubsExtension.class)
+public class ConfigUtilTest {
+
+    @SystemStub
+    private static EnvironmentVariables environmentVariables;
+
+
+    @Test
+    void check_config_default_value(){
+        String cfgName = UUID.randomUUID().toString();
+        String defValue = UUID.randomUUID().toString();
+        Assertions.assertEquals(defValue, ConfigUtil.getConfig(cfgName,defValue));
+    }
+
+    @Test
+    void check_config_env_value(){
+        String cfgName = UUID.randomUUID().toString();
+        String cfgValue = UUID.randomUUID().toString();
+        String defValue = UUID.randomUUID().toString();
+
+        environmentVariables.set(cfgName, cfgValue);
+
+        Assertions.assertEquals(cfgValue, ConfigUtil.getConfig(cfgName,defValue));
+    }
+
+    @Test
+    void check_config_sys_param_value(){
+        String cfgName = UUID.randomUUID().toString();
+        String cfgValue = UUID.randomUUID().toString();
+        String defValue = UUID.randomUUID().toString();
+
+        System.setProperty(cfgName,cfgValue);
+
+        Assertions.assertEquals(cfgValue, ConfigUtil.getConfig(cfgName,defValue));
+    }
+
+    @Test
+    void check_config_sys_param_over_env_value(){
+        String cfgName = UUID.randomUUID().toString();
+        String cfgValueSysParam = UUID.randomUUID().toString();
+        String cfgValueEnvVal = UUID.randomUUID().toString();
+        String defValue = UUID.randomUUID().toString();
+
+        System.setProperty(cfgName,cfgValueSysParam);
+        environmentVariables.set(cfgName, cfgValueEnvVal);
+
+        Assertions.assertEquals(cfgValueSysParam, ConfigUtil.getConfig(cfgName,defValue));
+    }
+
+}
