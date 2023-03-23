dcce5ca44e419c8c3a973137a880bc3b1e96f5ed
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-03-23 09:34:53 +0330

    add project structure

---
+++ spring-boot-3-rabbitmq/consumer/src/main/java/com/example/spring/cloud/stream/rabbitmq/consumer/ConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.cloud.stream.rabbitmq.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+}
