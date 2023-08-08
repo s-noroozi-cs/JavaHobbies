5df2af7a459405146a23278623371fa06aae105d
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-08-08 21:09:35 +0330

    Update AotApplication.java

--- spring-boot-native-image/src/main/java/com/example/aot/AotApplication.java
+++ spring-boot-native-image/src/main/java/com/example/aot/AotApplication.java
@@ -2,12 +2,22 @@ package com.example.aot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AotApplication.class, args);
+    }
 
+    @RequestMapping(method = RequestMethod.GET, path = "/sayHello/{name}")
+    public ResponseEntity<String> sayHello(@PathVariable("name") String name) {
+        return ResponseEntity.ok("Hello, %s".formatted(name));
+    }
 }
