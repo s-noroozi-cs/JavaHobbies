4d9af6b312d69e0b45dfd7fbd5dd0170bf660058
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-08-22 20:11:05 +0330

    Update MultipartRestApiApplication.java

--- spring-boot-multipart-rest/src/main/java/com/example/MultipartRestApiApplication.java
+++ spring-boot-multipart-rest/src/main/java/com/example/MultipartRestApiApplication.java
@@ -3,10 +3,14 @@ package com.example;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @SpringBootApplication
 @RestController
@@ -16,8 +20,11 @@ public class MultipartRestApiApplication {
     SpringApplication.run(MultipartRestApiApplication.class, args);
   }
 
-  @PostMapping("/upload")
-  public ResponseEntity upload(HttpServletRequest request) {
+  @PostMapping(value = "/upload")
+  public ResponseEntity upload(
+      @RequestParam(value = "file", required = false) MultipartFile file,
+      @RequestParam(value = "text", required = false) String Text,
+      HttpServletRequest request) {
     return ResponseEntity.ok("");
   }
 }
