33ec21618bf9452b1e45243b36303e4d0a17cb04
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-08-22 20:09:26 +0330

    Update MultipartRestApiAppTests.java

--- spring-boot-multipart-rest/src/test/java/com/example/MultipartRestApiAppTests.java
+++ spring-boot-multipart-rest/src/test/java/com/example/MultipartRestApiAppTests.java
@@ -4,6 +4,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -27,21 +38,38 @@ public class MultipartRestApiAppTests {
   }
 
   @Test
-  void invoke_empty_body_post() throws Exception {
-    Path file =
-        Path.of(
-            Thread.currentThread()
-                .getContextClassLoader()
-                .getResource("application.properties")
-                .toURI());
-    HttpResponse<String> response =
-        HttpClient.newHttpClient()
-            .send(
-                HttpRequest.newBuilder(getUploadUri())
-                    .POST(HttpRequest.BodyPublishers.ofFile(file))
-                    .build(),
-                HttpResponse.BodyHandlers.ofString());
-    Assertions.assertEquals("", response.body());
-    Assertions.assertEquals(200, response.statusCode());
+  void invoke_upload_file() {
+
+    HttpHeaders headers = new HttpHeaders();
+
+    // ContentType
+    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+    Resource file = new ByteArrayResource(new byte[] {123});
+
+    MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
+
+    // Common form parameters.
+    multipartBodyBuilder.part("text", "spring cloud");
+
+    // Custom ContentType.
+    multipartBodyBuilder.part(
+        "json",
+        """
+                [{"name": "open feign"}, {"name": "gateway"}]""",
+        MediaType.APPLICATION_JSON);
+
+    multipartBodyBuilder.part("file", file, MediaType.TEXT_PLAIN);
+
+    MultiValueMap<String, HttpEntity<?>> multipartBody = multipartBodyBuilder.build();
+
+    HttpEntity<MultiValueMap<String, HttpEntity<?>>> httpEntity =
+        new HttpEntity<>(multipartBody, headers);
+
+    RestTemplate restTemplate = new RestTemplate();
+    ResponseEntity<String> response =
+        restTemplate.postForEntity(getUploadUri(), httpEntity, String.class);
+
+    Assertions.assertTrue(response.getStatusCode().is2xxSuccessful());
   }
 }
