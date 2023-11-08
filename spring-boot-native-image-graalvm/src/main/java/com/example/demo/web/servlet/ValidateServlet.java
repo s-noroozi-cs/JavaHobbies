0c41fddee20666af0c27aacbdb33685b8ed985df
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-11-08 20:10:05 +0330

    Create ValidateServlet.java

---
+++ spring-boot-native-image-graalvm/src/main/java/com/example/demo/web/servlet/ValidateServlet.java
@@ -0,0 +1,10 @@
+package com.example.demo.web.servlet;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+public class ValidateServlet {
+
+  public void validate(HttpServletRequest request, HttpServletResponse response) {}
+}
