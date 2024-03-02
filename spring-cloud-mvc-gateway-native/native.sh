a0cc1bb0dac572a94fbe3c481802472a809f483c
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-03-02 19:49:20 +0330

    Update native.sh

--- spring-cloud-mvc-gateway-native/native.sh
+++ spring-cloud-mvc-gateway-native/native.sh
@@ -3,6 +3,5 @@
 #./mvnw clean -P native native:compile -DskipTests
 
 # set maven and java (graalvm withh native image tool in OS path variable)
-mvn -Pnative spring-boot:process-aot
 mvn -Pnative native:compile -DskipTests
 
