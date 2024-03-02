0d8c2bcf797d6cf13602685ee339d1748d189022
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-03-02 19:21:51 +0330

    create project structure

---
+++ spring-cloud-mvc-gateway-native/src/main/java/com/mvc/gateway/NativeMvcGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mvc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeMvcGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeMvcGatewayApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-mvc-gateway-native/src/test/java/com/mvc/gateway/NativeMvcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeMvcGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
