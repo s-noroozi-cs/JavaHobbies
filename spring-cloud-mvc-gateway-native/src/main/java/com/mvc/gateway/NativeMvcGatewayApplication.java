8b0264ca8c96c93e4dd9b03af23f2b3399792e1e
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-03-02 19:48:32 +0330

    Update NativeMvcGatewayApplication.java

--- spring-cloud-mvc-gateway-native/src/main/java/com/mvc/gateway/NativeMvcGatewayApplication.java
+++ spring-cloud-mvc-gateway-native/src/main/java/com/mvc/gateway/NativeMvcGatewayApplication.java
@@ -2,8 +2,9 @@ package com.mvc.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.server.mvc.GatewayServerMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = GatewayServerMvcAutoConfiguration.class)
 public class NativeMvcGatewayApplication {
 
 	public static void main(String[] args) {
