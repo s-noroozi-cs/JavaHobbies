2b0bf75265ab4bbf33881e10fd524af79130d9af
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-03-03 18:52:58 +0330

    Update native.sh

--- spring-boot-native-feign/native.sh
+++ spring-boot-native-feign/native.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 #check maven and graalvm (with native tool chain) in path of your operation correctly
-mvn clean -Pnative native:compile && ./tartget/native-feign
+mvn clean -Pnative native:compile && ./target/native-feign
