b084cea8b4225f1335347e771bdd2eba025d04c6
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-03-03 18:39:05 +0330

    create project structure

---
+++ spring-boot-native-feign/src/main/java/com/feign/nativefeign/NativeFeignApplication.java
@@ -0,0 +1,13 @@
+package com.feign.nativefeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeFeignApplication.class, args);
+	}
+
+}
