38433f67bfab66cd17371487c89288be0a924812
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-03-05 21:14:49 +0330

    remove structure and try again

--- spring-cloud-mvc-gateway-native/src/main/java/com/mvc/gateway/NativeMvcGatewayApplication.java
+++ spring-cloud-mvc-gateway-native/src/main/java/com/mvc/gateway/NativeMvcGatewayApplication.java
@@ -6,22 +6,19 @@ import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.server.mvc.GatewayServerMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
 @SpringBootApplication
-(exclude = GatewayServerMvcAutoConfiguration.class)
 public class NativeMvcGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NativeMvcGatewayApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(NativeMvcGatewayApplication.class, args);
+  }
 
   @Bean
   public RouterFunction<ServerResponse> getRoute() {
     return route().route(path("/uuid"), http("http://httpbin.org/uuid")).build();
   }
-
 }
