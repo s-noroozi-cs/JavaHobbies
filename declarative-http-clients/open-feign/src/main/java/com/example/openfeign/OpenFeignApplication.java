561c293bd1e8668a4a3a0f28711594fe44fe821e
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-03-08 15:08:10 +0330

    Update OpenFeignApplication.java

--- declarative-http-clients/open-feign/src/main/java/com/example/openfeign/OpenFeignApplication.java
+++ declarative-http-clients/open-feign/src/main/java/com/example/openfeign/OpenFeignApplication.java
@@ -1,14 +1,24 @@
 package com.example.openfeign;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@RestController
 public class OpenFeignApplication {
+  @Autowired FeignClientUUID feignClientUUID;
+
+  @GetMapping("open-feign/uuid")
+  public String openFeignUUID() {
+    return feignClientUUID.uuidGenerator();
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenFeignApplication.class, args);
