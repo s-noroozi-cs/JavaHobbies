8f593c8442d7b094c782f3e96787dfbe36e7c6f3
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-03-08 15:05:43 +0330

    Update UuidServiceApplication.java

--- declarative-http-clients/uuid-service/src/main/java/com/example/uuid/UuidServiceApplication.java
+++ declarative-http-clients/uuid-service/src/main/java/com/example/uuid/UuidServiceApplication.java
@@ -1,12 +1,21 @@
 package com.example.uuid;
 
+import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class UuidServiceApplication {
+
+  @GetMapping("/uuid")
+  public String uuidGenerator() {
+    return UUID.randomUUID().toString();
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(UuidServiceApplication.class, args);
