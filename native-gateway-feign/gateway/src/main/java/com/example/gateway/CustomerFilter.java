490539a7c1c2b38b56c086e96552dbe5382abb95
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-03-23 10:27:12 +0330

    Create CustomerFilter.java

---
+++ native-gateway-feign/gateway/src/main/java/com/example/gateway/CustomerFilter.java
@@ -0,0 +1,19 @@
+package com.example.gateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class CustomerFilter implements GlobalFilter, Ordered {
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    return chain.filter(exchange);
+  }
+
+  @Override
+  public int getOrder() {
+    return 0;
+  }
+}
