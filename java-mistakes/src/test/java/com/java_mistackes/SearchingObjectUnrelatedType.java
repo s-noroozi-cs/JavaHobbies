2c1ea5408b53af4e12dc6116952ee93f13c045b9
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-04-25 09:22:00 +0330

    Update SearchingObjectUnrelatedType.java

--- java-mistakes/src/test/java/com/java_mistackes/SearchingObjectUnrelatedType.java
+++ java-mistakes/src/test/java/com/java_mistackes/SearchingObjectUnrelatedType.java
@@ -1,5 +1,23 @@
 package com.java_mistackes;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
 public class SearchingObjectUnrelatedType {
-    
+
+  @Test
+  void searchUnrelatedType() {
+    Short one = 1, two = 2;
+    Set<Short> numbers = Set.of(one, two);
+
+    assertTrue(numbers.contains(one));
+    assertFalse(numbers.contains(2)); // care about boxed number
+    assertTrue(numbers.contains((short) 2));
+
+    assertTrue(one instanceof Short);
+    Object sumOfTwoShortValue = one + two; // care about boxed number
+    assertTrue(sumOfTwoShortValue instanceof Integer);
+  }
 }
