43655032bf3f2de70406ad0f1137e4616bc6ad70
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-04-26 08:20:45 +0330

    Update ConcurrentModification.java

--- java-mistakes/src/test/java/com/java_mistackes/ConcurrentModification.java
+++ java-mistakes/src/test/java/com/java_mistackes/ConcurrentModification.java
@@ -4,41 +4,59 @@ import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ConcurrentModification {
   @Test
-  void avoidConcurrentModification() {
-    List<String> letterList = new ArrayList<>(List.of("a", "b", "c"));
+  void for_each_add_new_item() {
+    List<String> list = new ArrayList<>(List.of("a", "b", "c"));
 
     Assertions.assertThrowsExactly(
         ConcurrentModificationException.class,
         () -> {
-          for (String letter : letterList)
-              if (letter.equals("b"))
-                  letterList.add("x");
+          for (String item : list) if (item.equals("b")) list.add("x");
         });
-
-    Assertions.assertThrowsExactly(
-        ConcurrentModificationException.class,
-        () -> {
-          Iterator<String> iterator = letterList.iterator();
-          while (iterator.hasNext())
-              if (iterator.next().equals("b"))
-                  letterList.add("x");
-        });
-
-    for (int i = 0; i < letterList.size(); i++)
-      if (letterList.get(i).equals("b"))
-          letterList.add("x");
-
-      List<String> list = new ArrayList<>(List.of("a", "b", "c"));
-      for (String s : list) {
-          System.out.println(s);
-          if (s.equals("b")) {
-              list.remove("a");
-          }
-      }
   }
+
+  @Test
+  void iterator_add_new_item() {
+    List<String> list = new ArrayList<>(List.of("a", "b", "c"));
+
+    Assertions.assertThrowsExactly(
+        ConcurrentModificationException.class,
+        () -> {
+          Iterator<String> iterator = list.iterator();
+          while (iterator.hasNext()) if (iterator.next().equals("b")) list.add("x");
+        });
+  }
+
+  @Test
+  void for_each_remove_existing_item() {
+      List<String> list = new ArrayList<>(List.of("a", "b", "c"));
+    List<String> traverseItems = new ArrayList<>();
+    for (String item : list) {
+      traverseItems.add(item);
+      if (item.equals("b")) {
+        list.remove("a");
+      }
+    }
+    Assertions.assertNotEquals("a,b,c", traverseItems.stream().collect(Collectors.joining(",")));
+  }
+
+    @Test
+    void iterator_remove_existing_item() {
+        List<String> list = new ArrayList<>(List.of("a", "b", "c"));
+        List<String> traverseItems = new ArrayList<>();
+        Iterator<String> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            String item = iterator.next();
+            traverseItems.add(item);
+            if (item.equals("b")) {
+                list.remove("a");
+            }
+        }
+        Assertions.assertNotEquals("a,b,c", traverseItems.stream().collect(Collectors.joining(",")));
+    }
 }
