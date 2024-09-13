7d7c49853194914f2742ab2ecf2f208fb4408faf
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-09-13 15:17:00 +0330

    create project structure

---
+++ spring-boot-csd-aot-with-build-pack/src/main/java/com/example/cds/csd_with_build_pack/CsdWithBuildPackApplication.java
@@ -0,0 +1,13 @@
+package com.example.cds.csd_with_build_pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsdWithBuildPackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsdWithBuildPackApplication.class, args);
+	}
+
+}
