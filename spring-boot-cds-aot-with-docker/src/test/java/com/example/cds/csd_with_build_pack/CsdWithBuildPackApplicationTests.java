c99f7f2cd287e5bbbe100753e7bb8a97d35f0da0
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-09-15 21:42:57 +0330

    create project structure

---
+++ spring-boot-cds-aot-with-docker/src/test/java/com/example/cds/csd_with_build_pack/CsdWithBuildPackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cds.csd_with_build_pack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsdWithBuildPackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
