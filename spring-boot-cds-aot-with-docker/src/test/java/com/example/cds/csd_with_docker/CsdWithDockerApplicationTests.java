3d0373746e127716e1433489dca571ebae4580f2
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-09-27 15:29:17 +0330

    Update CsdWithDockerApplicationTests.java

--- spring-boot-cds-aot-with-docker/src/test/java/com/example/cds/csd_with_docker/CsdWithDockerApplicationTests.java
+++ spring-boot-cds-aot-with-docker/src/test/java/com/example/cds/csd_with_docker/CsdWithDockerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.cds.csd_with_build_pack;
+package com.example.cds.csd_with_docker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
