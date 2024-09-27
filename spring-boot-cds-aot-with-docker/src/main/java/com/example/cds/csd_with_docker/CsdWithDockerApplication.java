266ea1f694d543087bfaefeb7a40a67bb5d6c5d5
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-09-27 15:11:13 +0330

    Update CsdWithDockerApplication.java

--- spring-boot-cds-aot-with-docker/src/main/java/com/example/cds/csd_with_docker/CsdWithDockerApplication.java
+++ spring-boot-cds-aot-with-docker/src/main/java/com/example/cds/csd_with_docker/CsdWithDockerApplication.java
@@ -1,4 +1,4 @@
-package com.example.cds.csd_with_build_pack;
+package com.example.cds.csd_with_docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
