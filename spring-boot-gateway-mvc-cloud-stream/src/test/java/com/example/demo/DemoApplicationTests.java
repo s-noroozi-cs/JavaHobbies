18910794b4cfa9d3bc2620ee79bd8a6fbb1f84f7
Saeid Noroozi <s.noroozi.cs@gmail.com>
2025-11-11 19:44:15 +0330

    update

--- spring-boot-gateway-mvc-cloud-stream/src/test/java/com/example/demo/DemoApplicationTests.java
+++ spring-boot-gateway-mvc-cloud-stream/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,36 +1,45 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class DemoApplicationTests {
 
-    @LocalServerPort
-    private int port;
+  @LocalServerPort private int port;
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {}
 
-    @Test
-    void verifySwaggerUi()throws Exception{
-        var client = HttpClient.newBuilder().build();
-        var uri = URI.create("http://localhost:"+port+"/swagger-ui.html");
-        var request = HttpRequest.newBuilder(uri).GET().build();
-        var response = client.send(request,
-                HttpResponse.BodyHandlers.ofString());
+  @Test
+  void verifyActuator() throws Exception {
+    var client = HttpClient.newBuilder().build();
+    var uri = URI.create("http://localhost:" + port + "/actuator/health");
+    var request = HttpRequest.newBuilder(uri).GET().build();
+    var response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-    Assertions.assertEquals(404, response.statusCode());
-    }
+    Assertions.assertEquals(200, response.statusCode());
+    Assertions.assertEquals("{\"status\":\"UP\"}", response.body());
+  }
 
+  @Test
+  void verifySwaggerUi() throws Exception {
+    var client = HttpClient.newBuilder().build();
+    var uri = URI.create("http://localhost:" + port + "/swagger-ui.html");
+    var request = HttpRequest.newBuilder(uri).GET().build();
+    var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+    Assertions.assertEquals(302, response.statusCode());
+    Assertions.assertEquals(
+        "/swagger-ui/index.html",
+            response.headers().allValues("location").getFirst());
+  }
 }
