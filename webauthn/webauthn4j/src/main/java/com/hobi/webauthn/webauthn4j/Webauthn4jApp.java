7d70f4caf51b657677ddab7acec70ca962106f5c
Saeid Noroozi <s.noroozi.cs@gmail.com>
2025-12-11 15:08:57 +0330

    update

---
+++ webauthn/webauthn4j/src/main/java/com/hobi/webauthn/webauthn4j/Webauthn4jApp.java
@@ -0,0 +1,11 @@
+package com.hobi.webauthn.webauthn4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webauthn4jApp {
+  public static void main(String[] args) {
+    SpringApplication.run(Webauthn4jApp.class, args);
+  }
+}
